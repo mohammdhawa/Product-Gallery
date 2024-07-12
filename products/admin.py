74eb99bc5f69cd2fdee53f9d4cdbad8a3dd0b07c
Muhammed Recep Hava <mohammdhawa>
2024-07-12 16:55:51 +0300

    Enhanced admin interface for Product model
    - Added ProductImages as an inline in ProductAdmin
    - Configured list display, filtering, and search fields for ProductAdmin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImages
 
 # Register your models here.
+
+admin.site.register(Category)
+
+
+class ProductImagesAdmin(admin.TabularInline):
+    model = ProductImages
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImagesAdmin, ]
+    list_display = ('name', 'code')
+    list_filter = ('price',)
+    search_fields = ('name', 'code')
+
+
+admin.site.register(Product, ProductAdmin)
